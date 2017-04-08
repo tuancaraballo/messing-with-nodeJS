0a74233bc020aa840aec0e6f13d0806d9d46035f
Antuan Caraballo <tranc2606@gmail.com>
2017-04-08 05:09:15 -0700

    fixing heroku

--- webserver.js
+++ webserver.js
@@ -8,10 +8,13 @@ var PORT = process.env.PORT || 3000;
 
 
 
-app.get('/', function (request, response) {
-	response.send('TODO API Root');
-});
+// --> The class had the index.html inside a public directory 
+//     so you might do  __dirname +'/public'
+app.use(express.static(__dirname));
+// console.log(__dirname);
 
+
+// At the end, you tell it to listen to specific port. 
 app.listen(PORT, function () {
-	console.log('Express listening on port ' + PORT + '!');
+	console.log('Server listering on port ' + PORT + ' ...');
 });
