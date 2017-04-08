0a03828d7f9e902ad36450f397fa2f4006fc597a
Antuan Caraballo <tranc2606@gmail.com>
2017-04-08 05:00:20 -0700

    fixing issues with heroku

--- webserver.js
+++ webserver.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
+
+/*		HEROKU LESSON
+ ---> Heroku establishes its own port, you can ask for this port thorugh the process.env.PORT 
+*/
 var PORT = process.env.PORT || 3000;
+
+
 
 app.get('/', function (request, response) {
 	response.send('TODO API Root');
