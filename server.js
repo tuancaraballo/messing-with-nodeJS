f57cd3bb8df82a2e58079bbf17a9e28c50c12e5d
Antuan Caraballo <tranc2606@gmail.com>
2017-04-12 06:22:02 -0700

    Add POST to todo items

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id: 1,
-	description: 'meet mom for lunch',
-	completed: false
-},
-	{
-	id: 2,
-	description: 'Go grocieries',
-	completed: false
-},
-	{
-	id: 3,
-	description: 'Grind!!',
-	completed: true
-},
-];
+var todos = [];
+var todoNextId = 1;
+
+//--> This is a middleware, remember that when you use
+// app.use all methods will call this first.
+
+
+app.use(bodyParser.json()); //--> it allows to parse JSON objects sent on the
+							// on the request body.
+							// Every time a JSON comes in, express will parse it
+							// and access it via request.body;
 
 
 app.get('/todos', function (request, response) {
+	response.sendfile('views/test.html', {root: __dirname })
 	response.json(todos);
 });
 
@@ -75,6 +73,39 @@ app.get('/todos/:id', function (request, response){
 		response.status(404).send();
 	}
 });
+
+/**
+	LESSON TIME: POST/todos and body parser, send JSON data in the body
+				 to server using POSTMAN
+
+ --> Bodyparser allows you to parse a JSON object when it comes 
+ 	 in the request.
+
+	1- npm install body-parser --save  #--> remember that the --save saves it into
+									the package.json, which allows
+									you to just run npm install without any
+									extra argumens if lose the node_modules
+
+    2- var bodyParse = require('body-parser');
+
+	3- Add at the beginnig:
+
+				app.use(bodyParser.json())
+
+    3- To send data to your server using POSTMAN, you first need to
+        change the method to POST, then go to the body tab, from the dropdown
+        menu right next to binary, which might show 'text' by default, pick 
+        JSON(application/json). Notice how Headers(1) shows 1 now
+**/
+app.post('/todos', function (request, response) {
+	var body = request.body;
+	body.id = todoNextId;
+	todos.push(body);
+	todoNextId++;
+	response.json(body);
+});
+
+
 
 // At the end, you tell it to listen to specific port. 
 app.listen(PORT, function () {
